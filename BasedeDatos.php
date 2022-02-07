589d8199435c4280440b5c04e4f4185dc2f28bbe
Jhostin <jhostin.cruz12346@gmail.com>
2022-02-07 14:33:43 -0600

    ya funciona el apartado de inicio de la pagina

---
+++ BasedeDatos.php
@@ -0,0 +1,13 @@
+<?php
+/*
+en esta hoja se incluye la conexion con la base de datos y tambien se modela la base de datos
+ya sea inicio de cesion u otras actividades que tenga que ver con la base de datos
+*/
+
+$user = "JhostinDelaC";
+$pass = "Jhostin2004@";
+$server = "localhost";
+$db = "bd_Sis";
+$con = new mysqli($server, $user, $pass, $db);
+
+
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Herramientas/estilos/bootstrap5/css/bootstrap.css">
+    <script src="jqueri/jquery.js"></script>
+
+</head>
+
+<body class="todo">
+    <div class="Menu">
+        <nav class="navbar navbar-light" style="background-color:#2c3e50;">
+            <div class="container-fluid">
+                <a class="navbar-brand"></a>
+                <form class="d-flex">
+                  <a href="inicio_Sesion.php"><button style="margin-right: 5px;" class="btn btn-outline-light" type="button">Iniciar sesion</button></a>
+                  <a href="Crear_Cuenta.php"><button class="btn btn-outline-light" type="button">Registrarse</button></a>
+                </form>
+            </div>
+        </nav>
+    </div>
+
+    <div class="container contenederTexto">
+        <div class="row">
+            <div class="col-sm-6 texto">
+            "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
+            </div>
+            <div class="col-sm-6">
+                imagenes
+            </div>
+        </div>
+        <div class="row"></div>
+    </div>
+
+
+    <script src="Herramientas/estilos/bootstrap5/js/bootstrap.js"></script>
+</body>
+
+</html>
